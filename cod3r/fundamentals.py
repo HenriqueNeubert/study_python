3882da2c1227445e3d020f9d3d612e78cf702f57
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-03-30 17:34:11 -0300

    feat: add archives commands.py and fundamentals.py

---
+++ cod3r/fundamentals.py
@@ -0,0 +1,54 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+###operadores aritméticos###
+
++ 
+-
+*
+/
+// resultado sempre inteiro
+** elevado
+% resto da divisão
+
+###operadores unários###
++= INC
+-= DEC
+not 1
+not 0
++a altera para um número positivo
+-a altera para um número negativo
+
+###operadores relacionais###
+>  
+>=
+<
+<=
+!=
+==
+
+###operadores de atribuição###
++= 
+-=
+*=
+/=
+%= resto da divisão
+**= elevado
+//= resultado sempre inteiro
+
+###operadores lógicos###
+and
+or
+xor exclusivamente tem que ser um ou o outro, jamais os dois
+not true
+not false
+
+###operadores ternários###
+esta_chovendo = false
+# 'malhadas' está mais perto do [], por isso seria false
+# já 'secas', está mais longe, por isso representa true
+ex:1
+print('Hoje está chovendo ' + ('secas', 'molhadas')[],)
+ex2:
+print('Hoje está chovendo ' + ('secas', if esta_chovendo else 'secas'))
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
